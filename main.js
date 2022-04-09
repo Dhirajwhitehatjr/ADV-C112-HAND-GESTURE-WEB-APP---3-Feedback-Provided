a22ee2005125018742ce937791ecd4100cf61031
Dhirajwhitehatjr <83785808+Dhirajwhitehatjr@users.noreply.github.com>
2022-04-09 15:11:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot() {
 
 console.log("ml5 version:",ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7xLL28ywN/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/to7dWjWVL/model.json',modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded Successfully!");
